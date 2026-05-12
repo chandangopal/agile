62673c59ea100ddcb5096586d863e57dbfbe0fe7
chandangopal <chandangopal144@gmail.com>
2026-05-12 21:11:31 +0530

    First Commit

---
+++ sub.py
@@ -0,0 +1,9 @@
+# Taking input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Calculating the sum
+total = num1 - num2
+
+# Displaying the result
+print(f"The difference of {num1} and {num2} is {total}")
